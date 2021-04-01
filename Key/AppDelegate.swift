657ed2843cbbcc7226af9b8fb6bcbf13a393ed75
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2021-04-01 13:37:40 +0800

    Initial Commit

---
+++ Key/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Key
+//
+//  Created by JiaChen(: on 1/4/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Key/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Key
+//
+//  Created by JiaChen(: on 1/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
